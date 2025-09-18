2186edff4adcd545f5805ab785c6449d6da9fd63
Lontong <2350241001@student.prasetiyamulya.ac.id>
2025-09-18 15:32:35 +0700

    Push

---
+++ Lab7-Leon/LabDjango/mysite/blog/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+class Mahasiswa(models.Model):
+    nim = models.IntegerField(null=True, blank=True)
+    firstname = models.CharField(max_length=100)
+    lastname = models.CharField(max_length=100)
+    jurusan = models.CharField(max_length=100)
+
+def __str__(self):
+    return f"{self.firstname} {self.lastname}"
